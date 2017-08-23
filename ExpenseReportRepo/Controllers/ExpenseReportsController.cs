eb3f51f6286e054233ac3010f7672fa4705d59ae
bricketts <brody.ricketts@gmail.com>
2017-08-22 18:11:30 -0600

    Added Authorize
    Added Authorize Attribute to the Expense Report Controller.

--- ExpenseReportRepo/Controllers/ExpenseReportsController.cs
+++ ExpenseReportRepo/Controllers/ExpenseReportsController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace ExpenseReportRepo.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class ExpenseReportsController : Controller
     {
         private readonly ExpenseReportRepoContext _context;
